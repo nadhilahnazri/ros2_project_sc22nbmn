79386e51a9e8c4d763c6fe18f81c2d2033a590b7
nadhilahnazri <sc22nbmn@leeds.ac.uk>
2025-04-02 17:32:23 +0100

    display not yet showing

--- ros2_project_sc22nbmn/final_step.py
+++ ros2_project_sc22nbmn/final_step.py
@@ -31,137 +31,107 @@ class Robot(Node):
         self.subscription  # prevent unused variable warning
 
         self.sensitivity = 10
-        self.box_detected = {'red': False, 'green': False, 'blue': False}
+        self.red_found = False
+        self.green_found = False
+        self.blue_found = False
 
+        self.publisher = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.rate = self.create_rate(10) #10Hz
+        self.too_close = False
+
+        
     def callback(self, data):
+        
         # Convert the received image into a opencv image
         # But remember that you should always wrap a call to this conversion method in an exception handler
         try:
             image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
-        except Exception as e:
-            self.get_logger().error(f'Error: {e}')
+        except CvBridgeError as e:
+            self.get_logger().error(str(e))
             return
+
+        cv2.namedWindow('camera_Feed',cv2.WINDOW_NORMAL)
+        cv2.imshow('camera_Feed', image)
+        cv2.resizeWindow('camera_Feed',320,240)
+        cv2.waitKey(3)
         
         # Set the upper and lower bounds for the two colours you wish to identify
         hsv_green_lower = np.array([60 - self.sensitivity, 100, 100])
         hsv_green_upper = np.array([60 + self.sensitivity, 255, 255])
-        hsv_red_lower = np.array([self.sensitivity, 100, 100])
-        hsv_red_upper = np.array([self.sensitivity, 255, 255])
+
+        hsv_red_lower1 = np.array([0, 100, 100])
+        hsv_red_upper1 = np.array([self.sensitivity, 255, 255])
+        hsv_red_lower2 = np.array([180 - self.sensitivity, 100, 100])
+        hsv_red_upper2 = np.array([180 + self.sensitivity, 255, 255])
+
         hsv_blue_lower = np.array([120 - self.sensitivity, 100, 100])
         hsv_blue_upper = np.array([120 + self.sensitivity, 255, 255])
 
         # Convert the rgb image into a hsv image
         hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-        # Filter out everything but a particular colour using the cv2.inRange() method
-
+        # Filter out everything but particular colours using the cv2.inRange() method
+        # Do this for each colour
+        green_mask = cv2.inRange(hsv_image, hsv_green_lower, hsv_green_upper)
+        blue_mask = cv2.inRange(hsv_image, hsv_blue_lower, hsv_blue_upper)
+        red_mask1 = cv2.inRange(hsv_image, hsv_red_lower1, hsv_red_upper1)
+        red_mask2 = cv2.inRange(hsv_image, hsv_red_lower2, hsv_red_upper2)
+        red_mask = cv2.bitwise_or(red_mask1, red_mask2)
+        
+        # mask_image = cv2.bitwise_or(green_image,red_image)
+        # #green_mask = cv2.bitwise_and(image,image,mask = green_image)
+        # #both_images = cv2.bitwise_or(green_mask,red_mask)
+        
+        # final_image = cv2.bitwise_and(image,image,mask = mask_image)
+        
+        
+        # To combine the masks you should use the cv2.bitwise_or() method
+        # You can only bitwise_or two images at once, so multiple calls are necessary for more than two colours
+        combined_mask = cv2.bitwise_or(green_mask, cv2.bitwise_or(blue_mask, red_mask))
 
         # Apply the mask to the original image using the cv2.bitwise_and() method
-        # As mentioned on the worksheet the best way to do this is to bitwise and an image with itself and pass the mask to the mask parameter
+        # As mentioned on the worksheet the best way to do this is to...
+        #bitwise and an image with itself and pass the mask to the mask parameter (rgb_image,rgb_image, mask=mask)
+        # As opposed to performing a bitwise_and on the mask and the image.
+        green_image = cv2.bitwise_and(image, image, mask=green_mask)
+        blue_image = cv2.bitwise_and(image, image, mask=blue_mask)
+        red_image = cv2.bitwise_and(image, image, mask=red_mask)
+        combined_image = cv2.bitwise_and(image, image, mask=combined_mask)
 
+        #Show the resultant images you have created. You can show all of them or just the end result if you wish to.
+        cv2.imshow('Original Feed', image)
+        cv2.imshow('Green Objects', green_image)
+        cv2.imshow('Blue Objects', blue_image)
+        cv2.imshow('Red Objects', red_image)
+        cv2.imshow('Combined Detection', combined_image)
 
-        # Find the contours that appear within the certain colour mask using the cv2.findContours() method
-        # For <mode> use cv2.RETR_LIST for <method> use cv2.CHAIN_APPROX_SIMPLE
-
-        # Loop over the contours
-        if len(contours)>0:
-
-            # There are a few different methods for identifying which contour is the biggest
-            # Loop through the list and keep track of which contour is biggest or
-            # Use the max() method to find the largest contour
-            
-            c = max(contours, key=cv2.contourArea)
-
-
-            #Check if the area of the shape you want is big enough to be considered
-            # If it is then change the flag for that colour to be True(1)
-            if cv2.contourArea(c) > x: #<What do you think is a suitable area?>
-                # Alter the value of the flag
-                myColourFlag = True
-
-        #Check if a flag has been set = colour object detected - follow the colour object
-        if self.colour1_flag == 1:
-            if cv2.contourArea(c) > aValue:
-                # Too close to object, need to move backwards
-                # Set a flag to tell the robot to move backwards when in the main loop
-                moveBackwardsFlag = True
-                
-            elif cv2.contourArea(c) < aValue:
-                # Too far away from object, need to move forwards
-                # Set a flag to tell the robot to move forwards when in the main loop
-                moveForwardsFlag = True
-            #else:
-                
-
-            # Be sure to do this for the other colour as well
-            # Setting the flag to detect blue, and stop the turtlebot from moving if blue is detected
-
-
-
-        # Show the resultant images you have created. You can show all of them or just the end result if you wish to.
-        cv2.namedWindow('camera_Feed',cv2.WINDOW_NORMAL)
-        cv2.imshow('camera_Feed', image)
-        cv2.resizeWindow('camera_Feed',320,240)
         cv2.waitKey(3)
-
-    def walk_forward(self):
-        #Use what you learnt in lab 3 to make the robot move forwards
-        desired_velocity = Twist()
-
-
-        for _ in range(30):  # Stop for a brief moment
-            self.publisher.publish(desired_velocity)
-            self.rate.sleep()
-
-    def walk_backward(self):
-        # Use what you learnt in lab 3 to make the robot move backwards
-        desired_velocity = Twist()
-
-
-        for _ in range(30):  # Stop for a brief moment
-            self.publisher.publish(desired_velocity)
-            self.rate.sleep()
-
-    def stop(self):
-        # Use what you learnt in lab 3 to make the robot stop
-        desired_velocity = Twist()
-
-
-        self.publisher.publish(desired_velocity)
 
 # Create a node of your class in the main and ensure it stays up and running
 # handling exceptions and such
 def main():
     def signal_handler(sig, frame):
-        robot.stop()
         rclpy.shutdown()
 
     # Instantiate your class
     # And rclpy.init the entire node
     rclpy.init(args=None)
     robot = Robot()
-    
 
-
+    # Ensure that the node continues running with rospy.spin()
+    # You may need to wrap rospy.spin() in an exception handler in case of KeyboardInterrupts
     signal.signal(signal.SIGINT, signal_handler)
     thread = threading.Thread(target=rclpy.spin, args=(robot,), daemon=True)
     thread.start()
 
     try:
         while rclpy.ok():
-            # Publish moves
-            #if found green:
-            #    if robot is too close:
-            #        move robot backward()
-            #    else:
-            #        robot walk forward()
-            pass
-
+            continue
     except ROSInterruptException:
         pass
-
     # Remember to destroy all image windows before closing node
     cv2.destroyAllWindows()
 
+# Check if the node is executing in the main path
 if __name__ == '__main__':
     main()
